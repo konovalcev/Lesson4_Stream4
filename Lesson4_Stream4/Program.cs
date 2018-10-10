7e8a6d18dba2a2e06b3e1ac6b4726d2c4270e943
aleks <akonovalcev@gmail.com>
2018-10-10 23:00:00 +0400

    Started Lesson4

---
+++ Lesson4_Stream4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson4_Stream4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
